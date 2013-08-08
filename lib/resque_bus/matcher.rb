ab24c32d4878e245487142d597def256bbadfb05
Brian Leonard <brian@bleonard.com>
2013-08-08 12:31:11 -0700

    matching on all attributes

--- lib/resque_bus/matcher.rb
+++ lib/resque_bus/matcher.rb
@@ -1,19 +1,44 @@
 module ResqueBus
   class Matcher
+    SPECIAL_PREPEND = "bus_special_value_"
     attr_reader :filters
     def initialize(hash)
-      @filters = hash
+      @filters = encode(hash)
     end
     
     def to_redis
       @filters
     end
     
-    def match?(attribute_name, given)
+    def match?(attribute_name, attributes)
       mine = filters[attribute_name].to_s
       return false if mine.size == 0
       
+      given = attributes[attribute_name]
+      case mine
+      when "#{SPECIAL_PREPEND}key"
+        return true if attributes.has_key?(attribute_name)
+        return false
+      when "#{SPECIAL_PREPEND}blank"
+        return true if given.to_s.strip.size == 0
+        return false
+      when "#{SPECIAL_PREPEND}empty"
+        return false if given == nil
+        return true if given.to_s.size == 0
+        return false
+      when "#{SPECIAL_PREPEND}nil"
+        return true if given == nil
+        return false
+      when "#{SPECIAL_PREPEND}value"
+        return false if given == nil
+        return true
+      when "#{SPECIAL_PREPEND}present"
+        return true if given.to_s.strip.size > 0
+        return false
+      end
+      
       given = given.to_s
+      
       return true if mine == given
       begin
         # if it's already a regex, don't mess with it
@@ -30,10 +55,26 @@ module ResqueBus
     end
     
     def matches?(attributes)
-      raise "no strings!" if attributes.to_s == attributes
+      return false if filters.empty?
+      return false if attributes == nil
       
-      # TODO: all filters
-      match?("bus_event_type", attributes["bus_event_type"])
+      filters.keys.each do |key|
+        return false unless match?(key, attributes)
+      end
+      
+      true
+    end
+    
+    def encode(hash)
+      out = {}
+      hash.each do |key, value|
+        case value
+        when :key, :blank, :nil, :present, :empty, :value
+          value = "#{SPECIAL_PREPEND}#{value}"
+        end
+        out[key.to_s] = value.to_s
+      end      
+      out
     end
   end
 end
---
+++ spec/matcher_spec.rb
@@ -0,0 +1,143 @@
+require 'spec_helper'
+
+module ResqueBus
+  describe Matcher do
+    it "should already return false on empty filters" do
+      matcher = Matcher.new({})
+      matcher.matches?({}).should  == false
+      matcher.matches?(nil).should  == false
+      matcher.matches?("name" => "val").should == false
+    end
+    
+    it "should not crash if nil inputs" do
+      matcher = Matcher.new("name" => "val")
+      matcher.matches?(nil).should == false
+    end
+    
+    it "string filter to/from redis" do
+      matcher = Matcher.new("name" => "val")
+      matcher.matches?("name" => "val").should   == true
+      matcher.matches?("name" => " val").should  == false
+      matcher.matches?("name" => "zval").should  == false
+    end
+    
+    it "regex filter" do
+      matcher = Matcher.new("name" => /^[cb]a+t/)
+      matcher.matches?("name" => "cat").should == true
+      matcher.matches?("name" => "bat").should == true
+      matcher.matches?("name" => "caaaaat").should == true
+      matcher.matches?("name" => "ct").should == false
+      matcher.matches?("name" => "bcat").should == false
+    end
+    
+    it "present filter" do
+      matcher = Matcher.new("name" => :present)
+      matcher.matches?("name" => "").should == false
+      matcher.matches?("name" => "cat").should == true
+      matcher.matches?("name" => "bear").should == true
+      matcher.matches?("other" => "bear").should == false
+    end
+    
+    it "blank filter" do
+      matcher = Matcher.new("name" => :blank)
+      matcher.matches?("name" => nil).should == true
+      matcher.matches?("other" => "bear").should == true
+      matcher.matches?("name" => "").should == true
+      matcher.matches?("name" => "  ").should == true
+      matcher.matches?("name" => "bear").should == false
+      matcher.matches?("name" => "   s ").should == false
+    end
+    
+    it "nil filter" do
+      matcher = Matcher.new("name" => :nil)
+      matcher.matches?("name" => nil).should == true
+      matcher.matches?("other" => "bear").should == true
+      matcher.matches?("name" => "").should == false
+      matcher.matches?("name" => "  ").should == false
+      matcher.matches?("name" => "bear").should == false
+    end
+    
+    it "key filter" do
+      matcher = Matcher.new("name" => :key)
+      matcher.matches?("name" => nil).should == true
+      matcher.matches?("other" => "bear").should == false
+      matcher.matches?("name" => "").should == true
+      matcher.matches?("name" => "  ").should == true
+      matcher.matches?("name" => "bear").should == true
+    end
+    
+    it "empty filter" do
+      matcher = Matcher.new("name" => :empty)
+      matcher.matches?("name" => nil).should == false
+      matcher.matches?("other" => "bear").should == false
+      matcher.matches?("name" => "").should == true
+      matcher.matches?("name" => "  ").should == false
+      matcher.matches?("name" => "bear").should == false
+      matcher.matches?("name" => "   s ").should == false
+    end
+    
+    it "value filter" do
+      matcher = Matcher.new("name" => :value)
+      matcher.matches?("name" => nil).should == false
+      matcher.matches?("other" => "bear").should == false
+      matcher.matches?("name" => "").should == true
+      matcher.matches?("name" => "  ").should == true
+      matcher.matches?("name" => "bear").should == true
+      matcher.matches?("name" => "   s ").should == true
+    end
+    
+    it "multiple filters" do
+      matcher = Matcher.new("name" => /^[cb]a+t/, "state" => "sleeping")
+      matcher.matches?("state" => "sleeping", "name" => "cat").should  == true
+      matcher.matches?("state" => "awake", "name" => "cat").should     == false
+      matcher.matches?("state" => "sleeping", "name" => "bat").should  == true
+      matcher.matches?("state" => "sleeping", "name" => "bear").should == false
+      matcher.matches?("state" => "awake", "name" => "bear").should    == false
+    end
+    
+    it "regex should go back and forth into redis" do
+      matcher = Matcher.new("name" => /^[cb]a+t/)
+      matcher.matches?("name" => "cat").should == true
+      matcher.matches?("name" => "bat").should == true
+      matcher.matches?("name" => "caaaaat").should == true
+      matcher.matches?("name" => "ct").should == false
+      matcher.matches?("name" => "bcat").should == false
+      
+      ResqueBus.redis.set("temp1", Resque.encode(matcher.to_redis))
+      redis = ResqueBus.redis.get("temp1")
+      matcher = Matcher.new(Resque.decode(redis))
+      matcher.matches?("name" => "cat").should == true
+      matcher.matches?("name" => "bat").should == true
+      matcher.matches?("name" => "caaaaat").should == true
+      matcher.matches?("name" => "ct").should == false
+      matcher.matches?("name" => "bcat").should == false
+      
+      ResqueBus.redis.set("temp2", Resque.encode(matcher.to_redis))
+      redis = ResqueBus.redis.get("temp2")
+      matcher = Matcher.new(Resque.decode(redis))
+      matcher.matches?("name" => "cat").should == true
+      matcher.matches?("name" => "bat").should == true
+      matcher.matches?("name" => "caaaaat").should == true
+      matcher.matches?("name" => "ct").should == false
+      matcher.matches?("name" => "bcat").should == false
+    end
+    
+    it "special value should go back and forth into redis" do
+      matcher = Matcher.new("name" => :blank)
+      matcher.matches?("name" => "cat").should == false
+      matcher.matches?("name" => "").should    == true
+      
+      ResqueBus.redis.set("temp1", Resque.encode(matcher.to_redis))
+      redis = ResqueBus.redis.get("temp1")
+      matcher = Matcher.new(Resque.decode(redis))
+      matcher.matches?("name" => "cat").should == false
+      matcher.matches?("name" => "").should    == true
+      
+      ResqueBus.redis.set("temp2", Resque.encode(matcher.to_redis))
+      redis = ResqueBus.redis.get("temp2")
+      matcher = Matcher.new(Resque.decode(redis))
+      matcher.matches?("name" => "cat").should == false
+      matcher.matches?("name" => "").should    == true
+    end
+  end
+end
