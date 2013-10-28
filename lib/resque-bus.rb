28128df48f1debf24afe191e5f1768caf2fb4bc7
Brian Leonard <brian@bleonard.com>
2013-10-28 11:05:03 -0700

    timezone crash protection

--- lib/resque-bus.rb
+++ lib/resque-bus.rb
@@ -152,8 +152,8 @@ module ResqueBus
       bus_attr = {"bus_published_at" => Time.now.to_i, "bus_event_type" => event_type}
       bus_attr["bus_id"]           = "#{Time.now.to_i}-#{generate_uuid}"
       bus_attr["bus_app_hostname"] = hostname
-      bus_attr["bus_locale"]       = I18n.locale.to_s if defined?(I18n) && I18n.respond_to?(:locale)
-      bus_attr["bus_timezone"]     = Time.zone.name   if defined?(Time) && Time.respond_to?(:zone)
+      bus_attr["bus_locale"]       = I18n.locale.to_s if defined?(I18n) && I18n.respond_to?(:locale) && I18n.locale
+      bus_attr["bus_timezone"]     = Time.zone.name   if defined?(Time) && Time.respond_to?(:zone)   && Time.zone
       bus_attr.merge(attributes || {})
     end
     
